08ff4eee7dc26fb4693b058473ee7f67a31cf98a
Daniel-627 <ochiengdaniel627@gmail.com>
2026-03-04 20:35:39 +0300

    feat: register all Unity AIC Sanity schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,16 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { memberAvatarSchema }    from './member-avatar'
+import { eventMediaSchema }      from './event-media'
+import { departmentMediaSchema } from './department-media'
+import { gallerySchema }         from './gallery'
+import { documentUploadSchema }  from './document-upload'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    memberAvatarSchema,
+    eventMediaSchema,
+    departmentMediaSchema,
+    gallerySchema,
+    documentUploadSchema,
+  ],
 }
